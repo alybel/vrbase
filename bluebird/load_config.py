5144600faded0ec824b604656fb5aeed31862bb8
vr <vr@vps211809.ovh.net>
2015-11-21 11:33:56 +0100

    changed load_config to talk to current database and added function for creating account folders if non existant yet

--- bluebird/load_config.py
+++ bluebird/load_config.py
@@ -1,8 +1,9 @@
 from sqlalchemy import schema, types, create_engine, orm
-
+import os.path
+import os
 
 def load_config(own_twittername=''):
-    connection_string = 'mysql+mysqldb://valureach:1ba12D1Kg84@62.75.156.31:3306/onboarding'
+    connection_string = 'mysql+pymysql://root:valuereachdb@localhost:3306/valuereach'
     eng = create_engine(connection_string)
     md = schema.MetaData(bind=eng, reflect=True)
     Session = orm.sessionmaker(bind=eng, autoflush=True, autocommit=False,
@@ -23,12 +24,19 @@ def load_config(own_twittername=''):
     for key in row:
         setattr(cfg, key, row[key])
 
-    cfg.keywords = dict(s.query(wl.c.keyword, wl.c.weight).filter(wl.c.email == cfg.email).all())
-    cfg.blacklist = dict(s.query(bl.c.keyword, bl.c.weight).filter(bl.c.email == cfg.email).all())
+    cfg.keywords = dict(s.query(wl.c.keyword, wl.c.weight).filter(wl.c.fk_user_id == cfg.fk_user_id).all())
+    cfg.blacklist = dict(s.query(bl.c.keyword, bl.c.weight).filter(bl.c.fk_user_id == cfg.fk_user_id).all())
     cfg.verbose = False
     cfg.languages = ['en', 'de']
     cfg.locations = []
     cfg.preambles = []
     cfg.hashtags = []
+    cfg.activity_frequency = 15
+    cfg.number_hashtags = 3
     print 'loaded config from database'
     return cfg
+
+def check_if_folder_exists_or_create(cfg):
+    directory = '../accounts/%s' % cfg.own_twittername
+    if not os.path.exists(directory):
+        os.makedirs(directory)

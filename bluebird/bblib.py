20f2ff8488828cce7c486a1c0bda8c68e4c1a10f
alybel <al.d.beck@gmail.com>
2015-12-06 22:11:17 +0100

    still trying to fix the memory leak

--- bluebird/bblib.py
+++ bluebird/bblib.py
@@ -283,8 +283,8 @@ def build_text(url):
     # choose preamble
     # build first part of text
     try:
-        #article_text, title = get_article_from_url(url)
-        article_text, title = 'machine learning, startup, ecommerce, fraud, risk, business', 'machine learning, startup, ecommerce, fraud, risk, business'
+        article_text, title = get_article_from_url(url)
+        #article_text, title = 'machine learning, startup, ecommerce, fraud, risk, business', 'machine learning, startup, ecommerce, fraud, risk, business'
     except Exception,e:
         logr.info('Failed to extracting article with goose in build_text. url was: %s, problem was: %s' % (url, e))
         return None, 0
--- bluebird/goose_manager.py
+++ bluebird/goose_manager.py
@@ -1,16 +1,9 @@
 __author__ = 'alex'
-from memory_profiler import profile
+from goose import Goose
+g = Goose()
 
-
-@profile
 def get_ws_text_and_title(url):
-    from goose import Goose
-    g = Goose()
     article = g.extract(url)
-
-    del g
-    del Goose
     a, b = "%s" % article.cleaned_text, "%s" % article.title
-    del article
-    return a, b
+    return None, None
 

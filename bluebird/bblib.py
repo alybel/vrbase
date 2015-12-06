e748b4d588ed8178028191af9f98ca085ef40c18
alybel <al.d.beck@gmail.com>
2015-12-06 16:11:01 +0100

    some more trials to remove memory leaks

--- bluebird/bblib.py
+++ bluebird/bblib.py
@@ -307,7 +307,7 @@ def build_text(url):
         print 'unicode error'
         score = -1
         hashtag_candidates = []
-
+    ##
     sorted_hts = sorted(hashtag_candidates.items(), key=operator.itemgetter(1), reverse=True)
     for i in xrange(3):
         old_text = "%s" % tweet_text
--- bluebird/goose_manager.py
+++ bluebird/goose_manager.py
@@ -4,9 +4,10 @@ def get_ws_text_and_title(url):
     from goose import Goose
     g = Goose()
     article = g.extract(url)
-    del article
+
     del g
     del Goose
-    a, b = article.cleaned_text, article.title
+    a, b = "%s" % article.cleaned_text, "%s" % article.title
+    del article
     return a, b
 

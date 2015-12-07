191423daf67c7bf34e60d704abea0a0d1b66c26f
alybel <al.d.beck@gmail.com>
2015-12-07 21:24:41 +0100

    still trying to fix the memory leak

--- bluebird/goose_manager.py
+++ bluebird/goose_manager.py
@@ -2,9 +2,10 @@ __author__ = 'alex'
 from goose import Goose
 g = Goose({'browser_user_agent': 'Mozilla', 'parser_class':'soup'})
 
-def get_ws_text_and_title(url):
+def get_ws_text_and_title(url = 'http://www.faz.net/aktuell/politik/kommentar-von-michaela-wiegel-zum-sieg-des-front-national-13953429.html'):
+
     article = g.extract(url)
     a, b = "%s" % article.cleaned_text, "%s" % article.title
     article = None
-    return a, b
+    return None, None
 

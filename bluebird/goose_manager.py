f53fa7714d0893f9b8a6defc8c4859365175b306
alybel <al.d.beck@gmail.com>
2015-12-07 20:43:20 +0100

    still trying to fix the memory leak

--- bluebird/goose_manager.py
+++ bluebird/goose_manager.py
@@ -5,5 +5,6 @@ g = Goose({'browser_user_agent': 'Mozilla', 'parser_class':'soup'})
 def get_ws_text_and_title(url):
     article = g.extract(url)
     a, b = "%s" % article.cleaned_text, "%s" % article.title
-    return None, None
+    article = None
+    return a, b
 
